2796b9a75c119dabcf8f8a24481c83b0519fc5cc
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-05-09 22:49:50 +0530

    added queue

--- structurepy/structurepy/__init__.py
+++ structurepy/structurepy/__init__.py
@@ -7,7 +7,10 @@ class stack:
         """
         basic constructor
         """
-        self.arr = np.ndarray([])
+        self.arr = np.ndarray([1,])
+    def __init__(self,array):
+        __init__(self)
+        np.append(self.arr,array)
     def push(self,data):
         """
         common push method. Add element to the top
@@ -26,3 +29,40 @@ class stack:
         return retval
     def __len__(self):
         return len(self.arr)-1
+
+
+class queue:
+    """
+    class queue for linear data structure
+    """
+    def __init__(self):
+        """
+        basic constructor
+        """
+        self.arr = np.ndarray([1,])
+    def __init__(self,array):
+        """
+        parameterized constructor
+        """
+        __init__(self)
+        np.append(self.arr,array)
+    def push(self,data):
+        """
+        common push method. Add element to the rear
+        """
+        self.arr = np.append(self.arr,data)
+    def pop(self):
+        """
+        common pop method. Removes front element
+        """
+        self.arr=self.arr[2:]
+    def __str__(self):
+        retval = '[ '
+        for a in self.arr[1:]:
+            retval+=str(a)+' '
+        retval+=']'
+        return retval
+    def __len__(self):
+        return len(self.arr)-1
+
+
